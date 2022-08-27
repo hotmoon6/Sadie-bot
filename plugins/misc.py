202606be8aad9c7c12879b5abddb20bf6a3739ef
Hotmoon <aravind1333.av@gmail.com>
2022-08-28 00:38:30 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -174,7 +174,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             imdb_id = imdb["imdb_id"],
             cast = imdb["cast"],
             runtime = imdb["runtime"],
-            countries = imdb["#countries"],
+            countries = imdb["countries"],
             certificates = imdb["certificates"],
             languages = imdb["languages"],
             director = imdb["director"],
