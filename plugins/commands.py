a18c4044143224cf3fff0e96e80d458d26ad0529
Hotmoon <aravind1333.av@gmail.com>
2022-08-28 22:59:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,7 +30,7 @@ async def start(client, message):
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+        await client.send_sticker(chat_id=message.chat.id, sticker='CAACAgUAAxkBAAEFtGFjC6VWcHyeh_pWhkKadVmClAEzggACcwcAApSAMVSdcHdRi8JyCSkE', reply_markup=reply_markup, reply_to_message_id=message.message_id)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
