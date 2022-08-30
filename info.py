806b7f8c0580f6483b04cc49e94e35cfdaeafaac
Hotmoon <aravind1333.av@gmail.com>
2022-08-30 18:10:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS', 'https://graph.org/file/7e56d907542396289fee4.jpg https://graph.org/file/9aa8dd372f4739fe02d85.jpg https://graph.org/file/adffc5ce502f5578e2806.jpg https://graph.org/file/6937b60bc2617597b92fd.jpg https://graph.org/file/09a7abaab340143f9c7e7.jpg https://graph.org/file/5a82c4a59bd04d415af1c.jpg https://graph.org/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
 
 # Admins, Channels & Users
@@ -40,18 +40,18 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Mindflayer_Files')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'arvinxoxo')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
-IMDB = is_enabled((environ.get('IMDB', "False")), False)
+IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<code>{file_name}</code>\n<b>Size:</b> {file_size}\n\n<b>⚠️ Provided by: [Sadie](https://t.me/mfsadiebot) </b>")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<code>{file_name}</code>\n<b>Size:</b> {file_size}\n{file_caption}\n\n<b>⚠️ Provided by: [Sadie](https://t.me/mfsadiebot) </b>")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "{title}\n{year} • {runtime} Minutes • {kind}\n\n🏷 Title: <a href={url}>{title} ({year})</a>\n🌟 Rating: {rating} / 10\n🎭 Genres: {genres}\n📆 Release info: {release_date}\n☀️ Language: {languages}\n🌍 Country: {countries}\n\n<b>⚠️ Powered by #MindflayersMirror</b>")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "{title}\n{year} • {kind} • {runtime} Minutes\n\n🏷 Title: <a href={url}>{title} ({year})</a>\n🌟 Rating: {rating} / 10\n🎭 Genres: {genres}\n📆 Release info: {release_date}\n☀️ Language: {languages}\n🌍 Country: {countries}\n\n<b>⚠️ Powered by #MindflayersMirror</b>")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "False"), False)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
+PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "True")), True)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), False)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
