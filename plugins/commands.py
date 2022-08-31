1a80ad3f813a5de94ea0174ac6c193ccf7e04d79
Hotmoon <aravind1333.av@gmail.com>
2022-08-31 21:45:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,14 +30,9 @@ async def start(client, message):
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
-            vix = await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.HOWTOUSE_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
-        await asyncio.sleep(7) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking...
-        await vix.delete()
+            vix = await message.reply_photo(photo=random.choice(PICS), caption=script.HOWTOUSE_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
+            await asyncio.sleep(7) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking...
+            await vix.delete()
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
