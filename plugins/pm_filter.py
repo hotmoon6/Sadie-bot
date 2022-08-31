a02c82c3b3101f4c2494bf4e15dfb70fa4c56dbd
Hotmoon <aravind1333.av@gmail.com>
2022-08-31 23:05:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -719,6 +719,7 @@ async def auto_filter(client, msg, spoll=False):
             saw = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
             await asyncio.sleep(15)
             await saw.delete()
+            return
         except Exception as e:
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
