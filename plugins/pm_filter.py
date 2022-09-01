8d3c2ca663450c17a473f425ce5c60bd8bd99230
Hotmoon <aravind1333.av@gmail.com>
2022-09-01 16:30:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -714,8 +714,10 @@ async def auto_filter(client, msg, spoll=False):
     if imdb and imdb.get('poster'):
         try:
             vs = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(5)
+            await asyncio.sleep(20)
             await vs.delete()
+            await message.delete()
+            return
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
