2f21822dde5d37d1e593ca9788d5e83ca0558d4a
Hotmoon <aravind1333.av@gmail.com>
2022-09-05 21:19:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -449,7 +449,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.HOWTOUSE_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.HOWTOUSE_TXT.format(message.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
