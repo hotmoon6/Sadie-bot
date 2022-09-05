d86966d39d6285817567b4f1a929a2216f58bbf9
Hotmoon <aravind1333.av@gmail.com>
2022-09-05 16:11:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,13 +44,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Vecna', url='https://t.me/Vecna_mirrorbot')
-            ],[
-            InlineKeyboardButton('💎 More', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+             InlineKeyboardButton('Explore', callback_data='explorefilter')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
