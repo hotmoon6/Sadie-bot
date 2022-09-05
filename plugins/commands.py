2a849e6217c0bfd1cd410b35dee805022793ca08
Hotmoon <aravind1333.av@gmail.com>
2022-09-05 20:49:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -26,8 +26,9 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         vix = await message.reply_photo(photo=random.choice(PICS), caption=script.HOWTOUSE_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
-        await asyncio.sleep(7) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking...
+        await asyncio.sleep(20) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking...
         await vix.delete()
+        await message.delete()
         return
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
