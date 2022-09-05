a387b394d27f825840289da229f5e4c905a1ea3f
Hotmoon <aravind1333.av@gmail.com>
2022-09-05 21:22:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -25,7 +25,7 @@ async def start(client, message):
             InlineKeyboardButton('Explore', callback_data='exploregroup')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        vix = await message.reply_photo(photo=random.choice(PICS), caption=script.HOWTOUSE_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
+        vix = await message.reply_photo(photo=random.choice(PICS), caption=script.LYRIC_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML)
         await asyncio.sleep(20) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking...
         await vix.delete()
         await message.delete()
