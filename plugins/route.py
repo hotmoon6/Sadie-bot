fe2f6c61856e18dfb9fba7711176db7acded65fb
Hotmoon <aravind1333.av@gmail.com>
2022-12-09 15:25:45 +0530

    Create route.py

---
+++ plugins/route.py
@@ -0,0 +1,14 @@
+#server
+
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Bot of Mindflayer's Mirror")
